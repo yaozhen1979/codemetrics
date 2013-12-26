c016bf8be941ae3642972a1e963cca5e1d37fd70
yaozhen <webber.larch@gmail.com>
2013-12-26 13:05:25 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codemetrics::Application.config.secret_key_base = '547f64c1a01079db9187db4dd8f6abf3fbf105478e232c9b770da47e613694d8a7045afb879e0dc90d1f5946ed07325bed3d59d61d9e99ec2177d7922644ade2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codemetrics::Application.config.session_store :cookie_store, key: '_codemetrics_session'
